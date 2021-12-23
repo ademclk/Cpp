1df7079da0c465279b3e228223e54146d026059f
Adem Onur ÇELİK <ademonurcelik@gmail.com>
2021-12-23 23:15:47 +0300

    Add files via upload

---
+++ Set1/2-consoleinteraction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    
+    string str;
+    cout << "Hello! What's your name?" << endl;
+    cin >> str;
+    cout << "Hello! " << str << " Nice to meet you." << endl;
+
+    return 0;
+}
